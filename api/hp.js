c1b37aae5a07de37a6e3210a9abf0ca4d4b31495
gabi <gabi0403m@gmail.com>
2026-04-09 08:57:25 -0300

    09/04

--- api/hp.js
+++ api/hp.js
@@ -1,25 +1,25 @@
 const prompt = require("prompt-sync")();
 
-async function listarFeiticos() {
+async function listarFeiticos() {// função pra listar somente os feitiços
   try {
     const res = await fetch("https://hp-api.onrender.com/api/spells");
     const data = await res.json();
 
     let inicio = 0;
-    const passo = 10;
+    const passo = 10; 
 
     while (inicio < data.length) {
 
       // mostra 10 feitiços
       data.slice(inicio, inicio + passo).forEach((f, i) => {
-        console.log(`\n${inicio + i + 1}. ${f.name}`);
-        console.log(`Descrição: ${f.description}`);
+        console.log(`\n${inicio + i + 1}. ${f.name}`); //mostra número e nome 
+        console.log(`Descrição: ${f.description}`);// mostra a descrição do feitiço
       });
 
       inicio += passo;
 
       // pergunta se quer continuar
-      const resposta = prompt("\nMostrar mais? (s/n): ").toLowerCase();
+      const resposta = prompt("\nMostrar mais? (s/n): ").toLowerCase(); // se quer continuar, mostra mais 10 feitiços
 
       if (resposta !== "s") {
         console.log("Encerrado.");
