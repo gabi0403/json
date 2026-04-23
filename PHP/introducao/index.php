458ec97399f1cf64fad0ef1e18a21d5e693b88de
gabi <gabi0403m@gmail.com>
2026-04-23 16:57:41 -0300

    23/04

---
+++ PHP/introducao/index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro PHP</title>
+</head>
+<body>
+
+    <h1> PHP abaixo: </h1>
+    <?php
+    echo "<h1>Isso é um titulo</h1>";
+    echo "Olá, visitante!👽 🖖";
+    echo "<strong> Isso é negrito </strong>";
+    // echo seria um print na tela
+    ?>
+
+</body>
+</html>
---
+++ PHP/introducao/paginaempresa.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página da empresa</title>
+</head>
+<body>
+    
+    <?php
+    echo "<h1>Bem-vindo ao site da empresa.</h1>";
+    echo "<p>Olá professor Jorge que vai corrigir isso</p>";
+    echo "<h3>Nossa empresa ofereçe serviços...... </h3>";
+    ?>
+
+</body>
+</html>
